54e6c9c3d92c48845dc48653789c50a9fe5f3baf
Mihir Kamdar <mihir.kamdar@couchbase.com>
2018-04-24 12:20:22 +0530

    high ops changes
    Change-Id: I49fd9043d06df5f91bae5e73a26e0c5985f6a99e

--- pytests/rebalance/rebalance_high_ops_pillowfight.py
+++ pytests/rebalance/rebalance_high_ops_pillowfight.py
@@ -11,7 +11,8 @@ from membase.helper.rebalance_helper import RebalanceHelper
 from remote.remote_util import RemoteMachineShellConnection
 from membase.helper.cluster_helper import ClusterOperationHelper
 from couchbase.bucket import Bucket
-from couchbase.exceptions import NotFoundError
+from couchbase.cluster import Cluster, PasswordAuthenticator
+from couchbase.exceptions import NotFoundError, CouchbaseError
 from lib.memcached.helper.data_helper import VBucketAwareMemcached
 
 
@@ -21,12 +22,60 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
         self.rate_limit = self.input.param("rate_limit",100000)
         self.batch_size = self.input.param("batch_size",1000)
         self.doc_size = self.input.param("doc_size",100)
+        self.loader = self.input.param("loader", "pillowfight")
+        self.instances = self.input.param("instances", 1)
 
     def tearDown(self):
         super(RebalanceHighOpsWithPillowFight, self).tearDown()
         self.sleep(120, "Wait till delete bucket completes")
 
     PREFIX = "test_"
+
+    def load_buckets_with_high_ops(self, server, bucket, items, batch=20000,
+                                   threads=5, start_document=0, instances=1):
+        import subprocess
+        cmd_format = "python scripts/high_ops_doc_loader.py  --node {0} --bucket {1} --user {2} --password {3} " \
+                     "--count {4} " \
+                     "--batch_size {5} --threads {6} --start_document {7}"
+        if instances > 1:
+            cmd = cmd_format.format(server.ip, bucket.name,
+                                    server.rest_username, server.rest_password,
+                                    int(items) / int(instances), batch, threads,
+                                    start_document)
+        else:
+            cmd = cmd_format.format(server.ip, bucket.name,
+                                    server.rest_username, server.rest_password,
+                                    items, batch,
+                                    threads, start_document)
+        if instances > 1:
+            for i in range(1, instances):
+                count = int(items) / int(instances)
+                start = count * i + int(start_document)
+                if i == instances - 1:
+                    count = items - (count * i)
+                cmd = "{} & {}".format(cmd,
+                                       cmd_format.format(server.ip, bucket.name,
+                                                         server.rest_username,
+                                                         server.rest_password,
+                                                         count, batch, threads,
+                                                         start))
+        self.log.info("Running {}".format(cmd))
+        result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+        output = result.stdout.read()
+        error = result.stderr.read()
+        if error:
+            self.log.error(error)
+            self.fail("Failed to run the loadgen.")
+        if output:
+            loaded = output.split('\n')[:-1]
+            total_loaded = 0
+            for load in loaded:
+                total_loaded += int(load)
+            self.assertEqual(total_loaded, items,
+                             "Failed to load {} items. Loaded only {} items".format(
+                                 items,
+                                 total_loaded))
 
     def load(self, server, items, batch=1000, docsize=100, rate_limit=100000):
         import subprocess
@@ -47,6 +96,87 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
         rc = subprocess.call(cmd, shell=True)
         if rc != 0:
             self.fail("Exception running cbc-pillowfight: subprocess module returned non-zero response!")
+
+    def load_docs(self, num_items=0, start_document=0):
+        if self.loader == "pillowfight":
+            load_thread = Thread(target=self.load,
+                                 name="pillowfight_load",
+                                 args=(
+                                 self.master, self.num_items, self.batch_size,
+                                 self.doc_size, self.rate_limit))
+            return load_thread
+        elif self.loader == "high_ops":
+            if num_items == 0:
+                num_items = self.num_items
+            load_thread = Thread(target=self.load_buckets_with_high_ops,
+                                 name="high_ops_load",
+                                 args=(self.master, self.buckets[0], num_items,
+                                       self.batch_size,
+                                       self.rate_limit, start_document,
+                                       self.instances))
+            return load_thread
+
+    def check_dataloss_for_high_ops_loader(self, server, bucket, num_items):
+
+        cluster = Cluster("couchbase://{}".format(server.ip))
+        auth = PasswordAuthenticator(server.rest_username, server.rest_password)
+        cluster.authenticate(auth)
+        bkt = cluster.open_bucket(bucket.name)
+        rest = RestConnection(self.master)
+        VBucketAware = VBucketAwareMemcached(rest, bucket.name)
+        _, _, _ = VBucketAware.request_map(rest, bucket.name)
+        batch_start = 0
+        batch_end = 0
+        batch_size = self.batch_size
+        errors = []
+        while num_items > batch_end:
+            if batch_start + batch_size > num_items:
+                batch_end = num_items
+            else:
+                batch_end = batch_start + batch_size
+            keys = []
+            for i in xrange(batch_start, batch_end, 1):
+                key = "Key_{}".format(i)
+                keys.append(key)
+            try:
+                result = bkt.get_multi(keys)
+                self.log.info(
+                    "Able to fetch keys starting from {0} to {1}".format(
+                        keys[0], keys[len(keys) - 1]))
+                for i in range(batch_start, batch_end):
+                    key = "Key_{}".format(i)
+                    value = {'val': i}
+                    if key in result:
+                        val = result[key].value
+                        for k in value.keys():
+                            if k in val and val[k] == value[k]:
+                                continue
+                            else:
+                                vBucketId = VBucketAware._get_vBucket_id(key)
+                                errors.append((
+                                              "Wrong value for key: {0}, VBucketId: {1}".format(
+                                                  key, vBucketId)))
+                    else:
+                        vBucketId = VBucketAware._get_vBucket_id(key)
+                        errors.append((
+                                      "Missing key: {0}, VBucketId: {1}".format(
+                                          key, vBucketId)))
+                self.log.info(
+                    "Validated key-values starting from {0} to {1}".format(
+                        keys[0], keys[len(keys) - 1]))
+            except CouchbaseError as e:
+                self.log.error(e)
+                ok, fail = e.split_results()
+                if fail:
+                    for key in fail:
+                        try:
+                            bkt.get(key)
+                        except NotFoundError:
+                            vBucketId = VBucketAware._get_vBucket_id(key)
+                            errors.append("Missing key: {0}, VBucketId: {1}".
+                                          format(key, vBucketId))
+            batch_start += batch_size
+        return errors
 
     def check_dataloss(self, server, bucket):
         bkt = Bucket('couchbase://{0}/{1}'.format(server.ip, bucket.name))
@@ -79,13 +209,17 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
             batch_start += batch_size
         return errors
 
+    def check_data(self, server, bucket, num_items=0):
+        if self.loader == "pillowfight":
+            return self.check_dataloss(server, bucket)
+        elif self.loader == "high_ops":
+            return self.check_dataloss_for_high_ops_loader(server, bucket,
+                                                           num_items)
+
     def test_rebalance_in(self):
         rest = RestConnection(self.master)
         bucket = rest.get_buckets()[0]
-        load_thread = Thread(target=self.load,
-                               name="pillowfight_load",
-                               args=(self.master, self.num_items, self.batch_size, self.doc_size, self.rate_limit))
-
+        load_thread = self.load_docs(num_items=self.num_items)
         self.log.info('starting the load thread...')
         load_thread.start()
         rebalance = self.cluster.async_rebalance(self.servers[:self.nodes_init],
@@ -93,7 +227,8 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
                                                  [])
         rebalance.result()
         load_thread.join()
-        errors = self.check_dataloss(self.master, bucket)
+        num_items_to_validate = self.num_items
+        errors = self.check_data(self.master, bucket, num_items_to_validate)
         if errors:
             self.log.info("Printing missing keys:")
         for error in errors:
@@ -108,17 +243,14 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
         self.log.info("Servers Out: {0}".format(servs_out))
         rest = RestConnection(self.master)
         bucket = rest.get_buckets()[0]
-        load_thread = Thread(target=self.load,
-                             name="pillowfight_load",
-                             args=(self.master, self.num_items, self.batch_size,
-                                   self.doc_size, self.rate_limit))
-
+        load_thread = self.load_docs(num_items=self.num_items)
         self.log.info('starting the load thread...')
         load_thread.start()
         rebalance = self.cluster.async_rebalance(self.servers[:self.nodes_init],[],servs_out)
         rebalance.result()
         load_thread.join()
-        errors = self.check_dataloss(self.master, bucket)
+        num_items_to_validate = self.num_items
+        errors = self.check_data(self.master, bucket, num_items_to_validate)
         if errors:
             self.log.info("Printing missing keys:")
         for error in errors:
@@ -134,11 +266,7 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
         self.log.info("Servers Out: {0}".format(servs_out))
         rest = RestConnection(self.master)
         bucket = rest.get_buckets()[0]
-        load_thread = Thread(target=self.load,
-                             name="pillowfight_load",
-                             args=(self.master, self.num_items, self.batch_size,
-                                   self.doc_size, self.rate_limit))
-
+        load_thread = self.load_docs(num_items=self.num_items)
         self.log.info('starting the load thread...')
         load_thread.start()
         rebalance = self.cluster.async_rebalance(self.servers[:self.nodes_init],
@@ -146,7 +274,8 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
                                                  self.nodes_init:self.nodes_init + self.nodes_in], servs_out)
         rebalance.result()
         load_thread.join()
-        errors = self.check_dataloss(self.master, bucket)
+        num_items_to_validate = self.num_items
+        errors = self.check_data(self.master, bucket, num_items_to_validate)
         if errors:
             self.log.info("Printing missing keys:")
         for error in errors:
---
+++ scripts/high_ops_doc_loader.py
@@ -0,0 +1,101 @@
+from couchbase.cluster import Cluster, PasswordAuthenticator
+from couchbase.bucket import Bucket, CouchbaseTransientError, KeyExistsError, LOCKMODE_WAIT
+import threading
+import argparse
+
+def parseArguments():
+  parser = argparse.ArgumentParser(description='Throw some docs in a bucket using the couchbase python client')
+  parser.add_argument('--threads', default=5, help="Number of threads to run")
+  parser.add_argument('--batch_size', default=5000, help="Batch size of eatch inserts")
+  parser.add_argument('--node', '-n', default="localhost", help='Cluster Node to connect to')
+  parser.add_argument('--bucket', '-b', default="default", help='Bucket to connect to')
+  parser.add_argument('--password', '-p',default="password", help='User password')
+  parser.add_argument('--user', '-u',default="Administrator", help='Username')
+  parser.add_argument('--prefix', '-k',default="CBPY_", help='Key Prefix')
+  parser.add_argument('--timeout', '-t', default=2, type=int, help='Operation Timeout')
+  parser.add_argument('--count', '-c', default=1000, type=int, help='Number of documents to insert')
+  parser.add_argument('--start_document', default=0, type=int, help="Starting document count to start loading from")
+  return parser.parse_args()
+
+
+args = parseArguments()
+host = args.node
+bucket_name = args.bucket
+user = args.user
+password = args.password
+cluster = Cluster("couchbase://{}".format(host))
+auth = PasswordAuthenticator(user, password)
+cluster.authenticate(auth)
+bucket = cluster.open_bucket(bucket_name, lockmode=LOCKMODE_WAIT)
+bucket.timeout = 5
+num_items = int(args.count)
+batch_size = int(args.batch_size)
+threads = int(args.threads)
+start_document = int(args.start_document)
+batches = []
+retry_batches = []
+num_completed = 0
+key_exists_error = 0
+thread_lock = threading.Lock()
+
+for i in range(start_document, start_document + num_items, batch_size):
+    if i + batch_size > start_document + num_items:
+        batches.append({"start": i, "end": start_document + num_items})
+    else:
+        batches.append({"start": i, "end": i + batch_size})
+
+def get_items(start, end):
+    items = {}
+    for x in range(start, end):
+        key = "Key_{}".format(x)
+        value = {'val': x}
+        items[key] = value
+    return items
+
+def get_retry_items():
+    items = {}
+    for item in retry_batches:
+        items[item] = {'val': int(item.split('_')[1])}
+    return items
+
+def insert_items(items):
+    try:
+        bucket.upsert_multi(items)
+        return items.__len__()
+    except CouchbaseTransientError as e:
+        ok, fail = e.split_results()
+        num_completed = ok.__len__()
+        for key in fail:
+            retry_batches.append(key)
+        return num_completed
+
+def insert_thread():
+    thread_lock.acquire()
+    if batches.__len__() > 0:
+        batch = batches.pop()
+    else:
+        thread_lock.release()
+        return 0
+    thread_lock.release()
+    items = get_items(batch['start'], batch['end'])
+    completed = insert_items(items)
+    thread_lock.acquire()
+    global num_completed
+    num_completed += completed
+    thread_lock.release()
+    return completed
+
+while batches:
+    threadpool = []
+    for i in range(0,threads):
+        t = threading.Thread(target=insert_thread)
+        t.start()
+        threadpool.append(t)
+    for t in threadpool:
+        t.join()
+while retry_batches:
+    items = get_retry_items()
+    retry_batches = []
+    completed = insert_items(items)
+    num_completed += completed
+print num_completed
