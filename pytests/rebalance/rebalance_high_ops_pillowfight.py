11fc11e85cbb4006ee0fe6fd421b877034fe4d31
Mihir Kamdar <mihir.kamdar@couchbase.com>
2018-04-25 14:54:02 +0530

    high ops changes
    Change-Id: I2280f776b1b4ce873e40755966ecef3ac82a0e34

--- pytests/rebalance/rebalance_high_ops_pillowfight.py
+++ pytests/rebalance/rebalance_high_ops_pillowfight.py
@@ -19,9 +19,9 @@ from lib.memcached.helper.data_helper import VBucketAwareMemcached
 class RebalanceHighOpsWithPillowFight(BaseTestCase):
     def setUp(self):
         super(RebalanceHighOpsWithPillowFight, self).setUp()
-        self.rate_limit = self.input.param("rate_limit",100000)
-        self.batch_size = self.input.param("batch_size",1000)
-        self.doc_size = self.input.param("doc_size",100)
+        self.rate_limit = self.input.param("rate_limit", 100000)
+        self.batch_size = self.input.param("batch_size", 1000)
+        self.doc_size = self.input.param("doc_size", 100)
         self.loader = self.input.param("loader", "pillowfight")
         self.instances = self.input.param("instances", 1)
         self.recovery_type = self.input.param("recovery_type", None)
@@ -81,25 +81,29 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
                                  items,
                                  total_loaded))
 
-    def load(self, server, items, batch=1000, docsize=100, rate_limit=100000, start_at=0):
+    def load(self, server, items, batch=1000, docsize=100, rate_limit=100000,
+             start_at=0):
         import subprocess
         from lib.testconstants import COUCHBASE_FROM_SPOCK
         rest = RestConnection(server)
         import multiprocessing
 
-        num_threads = multiprocessing.cpu_count()/2
-        num_cycles = int(items/batch * 1.5 / num_threads)
+        num_threads = multiprocessing.cpu_count() / 2
+        num_cycles = int(items / batch * 1.5 / num_threads)
 
-        cmd = "cbc-pillowfight -U couchbase://{0}/default -I {1} -m {3} -M {3} -B {2} -c {5} --sequential --json -t {4} --rate-limit={6} --start-at={7}"\
-            .format(server.ip, items, batch, docsize, num_threads, num_cycles, rate_limit, start_at)
-        #cmd = "cbc-pillowfight -U couchbase://{0}/default -I {1} -m {3} -M {3} -B {2} --populate-only --sequential --json -t {4} --rate-limit={6}" \
-        #        .format(server.ip, items, batch, docsize, num_cores/2, num_cycles, rate_limit)
+        cmd = "cbc-pillowfight -U couchbase://{0}/default -I {1} -m {3} -M {3} -B {2} -c {5} --sequential --json -t {4} --rate-limit={6} --start-at={7}" \
+            .format(server.ip, items, batch, docsize, num_threads, num_cycles,
+                    rate_limit, start_at)
+
+        if self.num_replicas > 0:
+            cmd += " --replicate-to=1"
         if rest.get_nodes_version()[:5] in COUCHBASE_FROM_SPOCK:
             cmd += " -u Administrator -P password"
         self.log.info("Executing '{0}'...".format(cmd))
         rc = subprocess.call(cmd, shell=True)
         if rc != 0:
-            self.fail("Exception running cbc-pillowfight: subprocess module returned non-zero response!")
+            self.fail(
+                "Exception running cbc-pillowfight: subprocess module returned non-zero response!")
 
     def load_docs(self, num_items=0, start_document=0):
         if num_items == 0:
@@ -108,8 +112,9 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
             load_thread = Thread(target=self.load,
                                  name="pillowfight_load",
                                  args=(
-                                 self.master, num_items, self.batch_size,
-                                 self.doc_size, self.rate_limit, start_document))
+                                     self.master, num_items, self.batch_size,
+                                     self.doc_size, self.rate_limit,
+                                     start_document))
             return load_thread
         elif self.loader == "high_ops":
             load_thread = Thread(target=self.load_buckets_with_high_ops,
@@ -162,13 +167,13 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
                             else:
                                 vBucketId = VBucketAware._get_vBucket_id(key)
                                 errors.append((
-                                              "Wrong value for key: {0}, VBucketId: {1}".format(
-                                                  key, vBucketId)))
+                                    "Wrong value for key: {0}, VBucketId: {1}".format(
+                                        key, vBucketId)))
                     else:
                         vBucketId = VBucketAware._get_vBucket_id(key)
                         errors.append((
-                                      "Missing key: {0}, VBucketId: {1}".format(
-                                          key, vBucketId)))
+                            "Missing key: {0}, VBucketId: {1}".format(
+                                key, vBucketId)))
                 self.log.info(
                     "Validated key-values starting from {0} to {1}".format(
                         keys[0], keys[len(keys) - 1]))
@@ -210,7 +215,9 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
                 keys.append(str(i).rjust(20, '0'))
             try:
                 bkt.get_multi(keys)
-                self.log.info("Able to fetch keys starting from {0} to {1}".format(keys[0], keys[len(keys)-1]))
+                self.log.info(
+                    "Able to fetch keys starting from {0} to {1}".format(
+                        keys[0], keys[len(keys) - 1]))
             except Exception as e:
                 self.log.error(e)
                 self.log.info("Now trying keys in the batch one at a time...")
@@ -231,6 +238,7 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
         elif self.loader == "high_ops":
             return self.check_dataloss_for_high_ops_loader(server, bucket,
                                                            num_items)
+
     '''
     def test_rebalance_in(self):
         rest = RestConnection(self.master)
@@ -280,8 +288,8 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
         if num_items_to_validate != rest.get_active_key_count(bucket):
             self.fail(
                 "FATAL: Data loss detected!! Docs loaded : {0}, docs present: {1}".
-                format(num_items_to_validate, rest.get_active_key_count(bucket)))
-
+                    format(num_items_to_validate,
+                           rest.get_active_key_count(bucket)))
 
     def test_rebalance_out(self):
         servs_out = [self.servers[self.nodes_init - i - 1] for i in
@@ -296,7 +304,8 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
         load_thread = self.load_docs(num_items=(self.num_items * 2),
                                      start_document=self.num_items)
         load_thread.start()
-        rebalance = self.cluster.async_rebalance(self.servers[:self.nodes_init],[],servs_out)
+        rebalance = self.cluster.async_rebalance(self.servers[:self.nodes_init],
+                                                 [], servs_out)
         rebalance.result()
         load_thread.join()
         num_items_to_validate = self.num_items * 3
@@ -308,7 +317,8 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
         if num_items_to_validate != rest.get_active_key_count(bucket):
             self.fail(
                 "FATAL: Data loss detected!! Docs loaded : {0}, docs present: {1}".
-                format(num_items_to_validate, rest.get_active_key_count(bucket)))
+                    format(num_items_to_validate,
+                           rest.get_active_key_count(bucket)))
 
     def test_rebalance_in_out(self):
         servs_out = [self.servers[self.nodes_init - i - 1] for i in
@@ -325,7 +335,8 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
         load_thread.start()
         rebalance = self.cluster.async_rebalance(self.servers[:self.nodes_init],
                                                  self.servers[
-                                                 self.nodes_init:self.nodes_init + self.nodes_in], servs_out)
+                                                 self.nodes_init:self.nodes_init + self.nodes_in],
+                                                 servs_out)
         rebalance.result()
         load_thread.join()
         num_items_to_validate = self.num_items * 3
@@ -337,7 +348,8 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
         if num_items_to_validate != rest.get_active_key_count(bucket):
             self.fail(
                 "FATAL: Data loss detected!! Docs loaded : {0}, docs present: {1}".
-                    format(num_items_to_validate, rest.get_active_key_count(bucket)))
+                    format(num_items_to_validate,
+                           rest.get_active_key_count(bucket)))
 
     def test_graceful_failover_addback(self):
         node_out = self.servers[self.node_out]
@@ -381,5 +393,86 @@ class RebalanceHighOpsWithPillowFight(BaseTestCase):
         if num_items_to_validate != rest.get_active_key_count(bucket):
             self.fail(
                 "FATAL: Data loss detected!! Docs loaded : {0}, docs present: {1}".
-                    format(num_items_to_validate, rest.get_active_key_count(bucket)))
+                    format(num_items_to_validate,
+                           rest.get_active_key_count(bucket)))
 
+    def test_multiple_rebalance_in_out(self):
+        servs_out = [self.servers[self.nodes_init - i - 1] for i in
+                     range(self.nodes_out)]
+        self.log.info("Servers Out: {0}".format(servs_out))
+        rest = RestConnection(self.master)
+        bucket = rest.get_buckets()[0]
+        load_thread = self.load_docs()
+        self.log.info('starting the initial load...')
+        load_thread.start()
+        load_thread.join()
+
+        self.log.info('starting the load before rebalance in...')
+        load_thread = self.load_docs(num_items=(self.num_items * 2),
+                                     start_document=self.num_items)
+        load_thread.start()
+
+        # Add 1 node
+        rebalance = self.cluster.async_rebalance(self.servers[:self.nodes_init],
+                                                 self.servers[
+                                                 self.nodes_init:self.nodes_init + self.nodes_in],
+                                                 [])
+        rebalance.result()
+        load_thread.join()
+        num_items_to_validate = self.num_items * 3
+        errors = self.check_data(self.master, bucket, num_items_to_validate)
+        if errors:
+            self.log.info("Printing missing keys:")
+        for error in errors:
+            print error
+        if num_items_to_validate != rest.get_active_key_count(bucket):
+            self.fail(
+                "FATAL: Data loss detected!! Docs loaded : {0}, docs present: {1}".
+                    format(num_items_to_validate,
+                           rest.get_active_key_count(bucket)))
+
+        self.log.info('starting the load before rebalance out...')
+        load_thread = self.load_docs(num_items=(self.num_items * 2),
+                                         start_document=self.num_items*3)
+
+        load_thread.start()
+        # Remove 1 node
+        rebalance = self.cluster.async_rebalance(self.servers[:self.nodes_init],
+                                                 [], servs_out)
+        rebalance.result()
+        load_thread.join()
+        num_items_to_validate = self.num_items * 5
+        errors = self.check_data(self.master, bucket, num_items_to_validate)
+        if errors:
+            self.log.info("Printing missing keys:")
+        for error in errors:
+            print error
+        if num_items_to_validate != rest.get_active_key_count(bucket):
+            self.fail(
+                "FATAL: Data loss detected!! Docs loaded : {0}, docs present: {1}".
+                    format(num_items_to_validate,
+                           rest.get_active_key_count(bucket)))
+
+        self.log.info('starting the load before swap rebalance...')
+        load_thread = self.load_docs(num_items=(self.num_items * 2),
+                                         start_document=self.num_items * 5)
+
+        load_thread.start()
+        # Swap rebalance 1 node
+        rebalance = self.cluster.async_rebalance(self.servers[:self.nodes_init],
+                                                 self.servers[
+                                                 self.nodes_init:self.nodes_init + self.nodes_in],
+                                                 servs_out)
+        rebalance.result()
+        load_thread.join()
+        num_items_to_validate = self.num_items * 7
+        errors = self.check_data(self.master, bucket, num_items_to_validate)
+        if errors:
+            self.log.info("Printing missing keys:")
+        for error in errors:
+            print error
+        if num_items_to_validate != rest.get_active_key_count(bucket):
+            self.fail(
+                "FATAL: Data loss detected!! Docs loaded : {0}, docs present: {1}".
+                    format(num_items_to_validate,
+                           rest.get_active_key_count(bucket)))
